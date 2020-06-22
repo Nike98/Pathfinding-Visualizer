2538849bdcddf875674adb5a390e776070e41d14
Nike98 <nikhilagarwal20@yahoo.com>
2020-06-22 23:38:00 +0530

    added all the rquired JavaScript for the Node usage

---
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./Node.css";
+
+export default class Node extends Component {
+  render() {
+    const {
+      row,
+      col,
+      isEnd,
+      isStart,
+      isWall,
+      isWeight,
+      onMouseClick,
+      onMouseEnter,
+    } = this.props;
+
+    const extraClassName = isEnd
+      ? "node-finish"
+      : isStart
+      ? "node-start"
+      : isWall
+      ? "node-wall"
+      : isWeight
+      ? "node-weight"
+      : "";
+
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onClick={() => onMouseClick(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+      ></div>
+    );
+  }
+}
